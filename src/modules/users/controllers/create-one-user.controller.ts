c8dc95c72f8c3e7452028f72c7de1edef4cb79a6
Guilherme Simoes Ferreira <guilhermesimfer@gmail.com>
2023-11-13 18:26:34 -0300

    refactor: make create one user a public controller

--- src/modules/users/controllers/create-one-user.controller.ts
+++ src/modules/users/controllers/create-one-user.controller.ts
@@ -1,11 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateOneUserService } from '../services';
 import { CreateOneUserDto } from '../dto';
+import { Public } from '../../../decorators';
 
 @Controller('users')
 export class CreateOneUserController {
   constructor(private readonly createOneUserService: CreateOneUserService) {}
 
+  @Public()
   @Post()
   async handle(@Body() dto: CreateOneUserDto) {
     return await this.createOneUserService.handle(dto);
